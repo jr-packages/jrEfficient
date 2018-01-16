1f6508142114ae438edbd7d6e501adeb1146aeb8
theoroe3 <theo@jumpingrivers.com>
2018-01-16 12:25:22 +0000

    removing course package instructions from solutions

--- vignettes/solutions1.R
+++ vignettes/solutions1.R
@@ -8,10 +8,10 @@ knit_theme$set(knit_theme$get("greyscale0"))
 
 options(replace.assign=FALSE,width=50)
 
-opts_chunk$set(fig.path='figure/graphics-', 
-               cache.path='cache/graphics-', 
-               fig.align='center', 
-               dev='pdf', fig.width=5, fig.height=5, 
+opts_chunk$set(fig.path='figure/graphics-',
+               cache.path='cache/graphics-',
+               fig.align='center',
+               dev='pdf', fig.width=5, fig.height=5,
                fig.show='hold', cache=FALSE, par=TRUE)
 knit_hooks$set(crop=hook_pdfcrop)
 
@@ -20,13 +20,6 @@ knit_hooks$set(par=function(before, options, envir){
     par(mar=c(3,3,2,1),cex.lab=.95,cex.axis=.9,
         mgp=c(2,.7,0),tcl=-.01, las=1)
   }}, crop=hook_pdfcrop)
-
-## ----eval=FALSE, tidy=FALSE---------------------
-#  install.packages("drat")
-
-## ----eval=FALSE, tidy=FALSE---------------------
-#  drat::addRepo("rcourses")
-#  install.packages("nclRefficient", type="source")
 
 ## -----------------------------------------------
 library("nclRefficient")
@@ -47,11 +40,11 @@ library("readr")
 #  ## 1. Using RData files is the fastest -
 #  ## although you have to read the data in first.
 #  ## Set colClasses also produces an good speed-up.
-#  
+#
 #  ##2. Setting colClasses R is no longer checking your data types.
 #  ## If your data is changing - for example it's coming from the web
 #  ## or a database, this may be problem.
-#  
+#
 #  ##3. The results do depend on the number of columns, as this code demonstrates
 #  N = c(1, 10, 100, 1000, 1000, 10000)
 #  l = numeric(5)
@@ -63,7 +56,7 @@ library("readr")
 #      read.csv("example.csv", colClasses=cc))[3]
 #  }
 #  l
-#  
+#
 #  ## Notice that when we have a large number of columns,
 #  ## we get a slow down in reading in data set (even though
 #  ## we have specified the column classes). The reason for
@@ -82,16 +75,16 @@ d_df = as.data.frame(d_m)
 colnames(d_df) = paste0("c", 1:ncol(d_df))
 
 ## ----results='hide', tidy=FALSE-----------------
-benchmark(replications=1000, 
+benchmark(replications=1000,
           d_m[1,], d_df[1,], d_m[,1], d_df[,1],
           columns=c("test", "elapsed", "relative"))
 
 ## ----echo=echo, results=results, tidy=TRUE------
 ## Two things are going on here
-## 1. The very large difference when selecting columns and rows (in data frames) 
+## 1. The very large difference when selecting columns and rows (in data frames)
 ## is because the data is stored in column major-order. Although the matrix is also stored in column major-order, because everything is the same type, we can efficiently select values.
 
-##2. Matrices are also more memory efficient: 
+##2. Matrices are also more memory efficient:
 m = matrix(runif(1e4), ncol=1e4)
 d = data.frame(m)
 object.size(m)
@@ -107,10 +100,10 @@ d_df[,colnames(d_df) == "c10"]
 #  benchmark(replications=10000,
 #            d_df$c10, d_df[,10], d_df[,"c10"],d_df[,colnames(d_df) == "c10"],
 #            columns=c("test", "elapsed", "relative"))
-#  
+#
 #  m = matrix(1:100000000, ncol=10000)
 #  dim(m)
-#  
+#
 #  benchmark(replications=10000,
 #            m[,1], m[1,], columns=c("test", "elapsed", "relative"))
 
