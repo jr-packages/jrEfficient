b2780cf0e965e4ab0609cd2c0ac6031825e6cea7
JohnMcIntyreJR <john@jumpingrivers.com>
2020-02-14 11:26:23 +0000

    Update Travis and inteRgrate checks

--- vignettes/solutions.R
+++ vignettes/solutions.R
@@ -7,8 +7,8 @@ library("bench")
 
 ## ----practical1, cache=TRUE, echo = TRUE----
 N = 1e5
-m = as.data.frame(matrix(runif(N), ncol=1000))
-write.csv(m, file="example.csv", row.names=FALSE)
+m = as.data.frame(matrix(runif(N), ncol = 1000))
+write.csv(m, file = "example.csv", row.names = FALSE)
 
 ## ----cache=TRUE, echo = TRUE---------------
 dd = read.csv("example.csv")
@@ -17,12 +17,12 @@ dd = read.csv("example.csv")
 system.time(read.csv("example.csv"))
 
 ## ----cache=TRUE,results='hide', tidy=FALSE, echo = TRUE----
-read.csv(file="example.csv",
-         colClasses=rep("numeric", 1000))
+read.csv(file = "example.csv",
+         colClasses = rep("numeric", 1000))
 
 ## ----cache=TRUE, results="hide", echo = TRUE----
-saveRDS(m, file="example.RData")
-readRDS(file="example.RData")
+saveRDS(m, file = "example.RData")
+readRDS(file = "example.RData")
 
 ## ----eval=FALSE, echo = TRUE---------------
 #  install.packages("readr")
@@ -34,15 +34,16 @@ readRDS(file="example.RData")
 #  ## 1. Using RData files is the fastest -
 #  ## although you have to read the data in first.
 #  ## Set colClasses also produces an good speed-up.
-#  
+#
 #  ##2. Setting colClasses R is no longer checking your data types.
 #  ## If your data is changing - for example it's coming from the web
 #  ## or a database, this may be problem.
-#  
-#  ##3. The results do depend on the number of columns, as this code demonstrates
+#
+#  ##3. The results do depend on the number of columns, as this code
+#  demonstrates
 #  N = c(1, 10, 100, 1000, 1000, 10000)
 #  l = numeric(5)
-#  for(i in seq_along(N)){
+#  for(i in seq_along(N)) {
 #    m = as.data.frame(matrix(runif(N[6]), ncol=N[i]))
 #    write.csv(m, file="example.csv", row.names=FALSE)
 #    cc = rep("numeric", N[i])
@@ -50,7 +51,7 @@ readRDS(file="example.RData")
 #      read.csv("example.csv", colClasses=cc))[3]
 #  }
 #  l
-#  
+#
 #  ## Notice that when we have a large number of columns,
 #  ## we get a slow down in reading in data set (even though
 #  ## we have specified the column classes). The reason for
@@ -61,34 +62,36 @@ readRDS(file="example.RData")
 ##For fast computers
 #d_m = matrix(1:1000000, ncol=1000)
 ##Slower computers
-d_m = matrix(1:10000, ncol=100)
+d_m = matrix(1:10000, ncol = 100)
 dim(d_m)
 
 ## ---- echo = TRUE--------------------------
 d_df = as.data.frame(d_m)
-colnames(d_df) = paste0("c", 1:ncol(d_df))
+colnames(d_df) = paste0("c", seq_along(d_df))
 
 ## ----results='hide', tidy=FALSE, echo = TRUE----
-mark(d_m[1,], d_df[1,], d_m[,1], d_df[,1],
-     relative = TRUE, 
+mark(d_m[1, ], d_df[1, ], d_m[, 1], d_df[, 1],
+     relative = TRUE,
      check = FALSE)
 
 ## ----  tidy=TRUE---------------------------
 ## Two things are going on here
 ## 1. The very large difference when selecting columns and rows (in data frames)
-## is because the data is stored in column major-order. Although the matrix is also stored in column major-order, because everything is the same type, we can efficiently select values.
+## is because the data is stored in column major-order. Although the matrix is
+## also stored in column major-order, because everything is the same type, we
+## can efficiently select values.
 
 ##2. Matrices are also more memory efficient:
-m = matrix(runif(1e4), ncol=1e4)
+m = matrix(runif(1e4), ncol = 1e4)
 d = data.frame(m)
 object.size(m)
 object.size(d)
 
 ## ----results='hide', echo = TRUE-----------
 d_df$c10
-d_df[,10]
-d_df[,"c10"]
-d_df[,colnames(d_df) == "c10"]
+d_df[, 10]
+d_df[, "c10"]
+d_df[, colnames(d_df) == "c10"]
 
 ## ---- eval=FALSE---------------------------
 #  mark(d_df$c10,
@@ -97,10 +100,10 @@ d_df[,colnames(d_df) == "c10"]
 #       d_df[,colnames(d_df) == "c10"],
 #       iterations = 10000,
 #       relative = TRUE)
-#  
+#
 #  m = matrix(1:100000000, ncol=10000)
 #  dim(m)
-#  
+#
 #  mark(m[,1], m[1,],
 #       iterations = 10000,
 #       relative = TRUE)
@@ -110,26 +113,26 @@ n = 1
 
 ## ----tidy=FALSE, echo = TRUE---------------
 a = NULL
-for(i in 1:n)
+for (i in 1:n)
   a = c(a, 2 * pi * sin(i))
 
 ## ----cache=TRUE, tidy=FALSE, echo = TRUE----
 n = 1e6
 ## Example 1
 I = 0
-for(i in 1:n) {
+for (i in 1:n) {
   10
   I = I + 1
 }
 ## Example 2
 I = 0
-for(i in 1:n){
+for (i in 1:n) {
   ((((((((((10))))))))))
   I = I + 1
 }
 ## Example 3
 I = 0
-for(i in 1:n){
+for (i in 1:n) {
   ##This is a comment
   ##But it is still parsed
   ##So takes time
@@ -146,4 +149,3 @@ detectCores()
 ## ----eval=FALSE, echo = TRUE---------------
 #  library("jrEfficient")
 #  vignette("solutions1", package="jrEfficient")
-
